36eeb3f1982310075df8c33d249a3cce865fb228
Tejaskt <tejasktgeetanjali88@gmail.com>
2025-08-25 20:11:29 +0530

    request handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require('express')
 
 const app = express()
 
-app.use((req,res)=>{
+app.use((req,res,next)=>{
     res.send("hello from the server!!!")
-})
+    next()
+},(req,res,next)=>{
+    // req handle 2 
+    res.send("hello from req handle 2")
+}
+)
 
 app.listen(3001,() => {
     console.log("server is started in port 3001")
