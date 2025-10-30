b0dd5a569b4c3ce19c89d19314d634b4d554a060
Tejaskt <tejasktgeetanjali88@gmail.com>
2025-10-30 18:45:33 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,46 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type : String
+        type : String,
+        required: true
     },
     lastName: {
         type : String
     },
     emailId: {
-        type : String
-    },
+        type : String,
+        unique: true,
+        required: true
+ },
     password: {
-        type : String
+        type : String,
+        required: true
     },
     age: {
         type : Number
     },
     gender: {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male","female","other"].includes(value.toLowerCase())){
+                throw new Error("Invalid gender data")
+            }
+        }
+    },
+    photoUrl: {
+        type : String,
+        default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-High-Quality-Image.png"
+    },
+    about: {
+        type : String,
+        default: "Hey there! I am using DevTinder app."
+
+    },
+    skills: {
+        type : [String]
     }
+},{
+    timestamps: true
 })
 
 module.exports = mongoose.model("User",userSchema)
