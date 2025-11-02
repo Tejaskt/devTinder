042703b183afd0dcf3014385e461df48c3dee83d
Tejaskt <tejasktgeetanjali88@gmail.com>
2025-11-02 22:56:58 +0530

    login api

--- src/app.js
+++ src/app.js
@@ -37,6 +37,27 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+app.post("/login", async(req,res) => {
+
+    try{
+        
+        const { emailId, password } = req.body
+
+        const user = await User.findOne({ emailId: emailId })
+        if(!user){
+            throw new Error("user is not present")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(isPasswordValid){
+            res.send("Login Successfull...!!")
+        }else{
+            throw new Error("password is not correct...!!")
+        }
+    }catch(err){
+        res.status(400).send("error Login the User: " + err.message)
+    }
+})
 
 
 connectDB().then(() => {
